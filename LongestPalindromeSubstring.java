6ca22d89bf357ab024d7703dd1189300f0d3ce59
kraken200 <krishnakanthguur@gmail.com>
2024-02-17 04:31:32 -0600

    added longest palindrome substring

--- LongestPalindromeSubstring.java
+++ LongestPalindromeSubstring.java
@@ -1,10 +1,5 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-
-class Program {
-public String longestPalindrome(String s) {
+class LongestPalindromeSubstring {
+    public String longestPalindrome(String s) {
         int max=1;
         int start=0;
         boolean[][] dp=new boolean[s.length()][s.length()];
@@ -16,10 +11,11 @@ public String longestPalindrome(String s) {
                 max=2;
             }
         }
-        for(int k=3;k<s.length();k++){
+        for(int k=3;k<=s.length();k++){
             for(int i=0;i<s.length()-k+1;i++){
                 int j=i+k-1;
                 if(dp[i+1][j-1] && s.charAt(i)==s.charAt(j)){
+                    dp[i][j]=true;
                     if(k>max){
                         start=i;
                         max=k;
@@ -28,10 +24,5 @@ public String longestPalindrome(String s) {
             }
         }
         return s.substring(start,start+max);
-    }
-    public static void main(String[] args) {
-        
-        Program p=new Program();
-        p.longestPalindrome("ccc");
     }
 }
